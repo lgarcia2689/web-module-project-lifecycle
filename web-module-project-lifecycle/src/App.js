75b4801d2bcf5294419cc3d084a173e0ee551f14
unknown <you@example.com>
2021-04-06 20:20:01 -0500

    created github cards

---
+++ web-module-project-lifecycle/src/App.js
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  
+  state = {
+    myCard:'',
+    followers:[]
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/lgarcia2689')
+            .then(res=>{
+                this.setState({
+                    myCard: res.data
+                });
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+     axios.get('https://api.github.com/users/lgarcia2689/followers')
+            .then(res=>{
+              console.log(res.data)
+                this.setState({
+                  followers: res.data
+                });
+            })
+            .catch(err=>{
+                console.log(err);
+            })        
+  }
+
+  render() {
+    return(  
+      <div className = "container">
+        <div className="header">
+      <p>My info : {this.state.myCard.name}</p>
+      <img width= "80"src = {this.state.myCard.avatar_url}/>
+      <p>Location: {this.state.myCard.location}</p>
+      <p>Public Repos: {this.state.myCard.public_repos}</p>
+      </div>
+
+      <div className="header">
+     
+      </div>
+   
+      
+        <div className = 'cards'>
+        <h1>Followers: {this.state.myCard.followers}</h1>
+                {
+                    this.state.followers.map(follower => {
+                        return (
+                          <div className = "card">
+                            <h3>Login : {follower.login}</h3>
+                            <img src = {follower.avatar_url} width="50"/>
+                            <p>Type: {follower.type}</p>
+                          </div>
+                        );
+                    })
+                }
+           </div>
+          
+
+
+  </div>
+    );
+  } 
+}
+export default App;
